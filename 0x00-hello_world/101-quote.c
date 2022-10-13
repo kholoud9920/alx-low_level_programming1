3527ea69633cec4ecab7c7c33304cc17ddce773c
kholoud9920 <kholoud9920@gmail.com>
2022-10-13 11:06:49 -0700

    q

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,9 @@
 #include<unistd.h>
-
-
-
 /**
- *
- *  * Description: main - prints the standrad error sandwiched around
- *
- *   * the standard outpu/t
- *
- *    * Return: returns 1
- *
- *     */
+ *   Description: main - prints the standrad error sandwiched around
+ * the standard outpu/t
+ * Return: returns 1
+ */
 
 
 
@@ -19,13 +12,7 @@ int main(void)
 
 
 {
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-
-
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-
-
-			return (1);
-
-
+	return (1);
+}

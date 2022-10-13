b1d2d5b49093c31df9aa16e26529c0a7cd15dbcf
kholoud9920 <kholoud9920@gmail.com>
2022-10-13 10:07:51 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+  #include<stdio.h>
+  /**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+  int main(void)
+  {
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+  	printf("Size of a char:%lubyte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a int:%lubyte(s)\n", (unsigned long)sizeof(i));
+ 	printf("Size of a long int:%lubyte(s)\n", (unsigned long)sizeof(li));
+ 	printf("Size of a long long int:%lubyte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float:%lubyte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+  }

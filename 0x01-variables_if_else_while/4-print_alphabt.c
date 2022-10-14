46ff678d3e81598040f68db9e75ba477b61b82d4
kholoud9920 <kholoud9920@gmail.com>
2022-10-14 11:06:53 -0700

    no q and o

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-print alphapet without q and e
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}

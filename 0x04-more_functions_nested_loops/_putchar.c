a9cfd077a9f10cf9a0816e4f04cca66460a1371c
kholoud9920 <kholoud9920@gmail.com>
2022-10-20 13:30:40 -0700

    num

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  *_putchar - display character
+  *@c: char used
+  *Return: char
+  */
+int _putchar(char c)
+{
+	return(c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,5 @@ int main(void);
 int _isupper(int c);
 int _isdigit(int x);
 int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);

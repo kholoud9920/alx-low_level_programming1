91bb37bc5736ebc2e66b352d72f13d4868b37efd
kholoud9920 <kholoud9920@gmail.com>
2022-10-20 11:51:20 -0700

    d

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *_isdigit- check number from0 to 9
+  *@x: the number to be checked
+  *Return: 1 for 0-9 num else 0
+  */
+int _isdigit(int x)
+{
+	if (x >= 48 && x <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}

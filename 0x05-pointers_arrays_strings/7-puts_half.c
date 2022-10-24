cc91977e2d604db9e9018954d1f458e268112384
kholoud9920 <kholoud9920@gmail.com>
2022-10-24 10:33:07 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,11 +6,12 @@
 void puts_half(char *str)
 {
 	int index = 0, len = 0, n;
+
 	while (str[index++])
 		len++;
 	if ((len % 2) == 0)
 		n = len / 2;
-	else 
+	else
 		n = (len + 1) / 2;
 	for (index = n; index < len; index++)
 		_putchar(str[index]);

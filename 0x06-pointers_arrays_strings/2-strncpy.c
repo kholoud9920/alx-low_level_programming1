b645196c6a9b8ddb5008f31ece55fee6c652a97a
kholoud9920 <kholoud9920@gmail.com>
2022-10-27 12:55:33 -0700

    g

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,12 @@
 * @n: The maximum number of bytes to copied from src.
 * Return: A pointer to the resulting string dest
  */
-char *_strncpy(char *dest, char *src, size_t n)
+char *_strncpy(char *dest, const char *src, size_t n)
 {
 	size_t i;
 
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
-	return (dest);
+	return dest;
 }

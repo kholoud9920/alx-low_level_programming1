32b858eef9b7e8d08dcd64f03ac96a412129f720
kholoud9920 <kholoud9920@gmail.com>
2022-11-04 12:21:33 -0700

    g

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
    * main - prints the number of arguments passed into it
    * @argc: argument counter
     * @argv: argument vector
-    * Return: 0 
+    * Return: 0
  */
 int main(int argc, char *argv[])
 {

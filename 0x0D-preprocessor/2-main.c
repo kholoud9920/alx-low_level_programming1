0258b9ccf7b30d9c054860ffbc1353dac1beac52
kholoud9920 <kholoud9920@gmail.com>
2022-11-14 10:04:03 -0800

    m

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,9 +3,9 @@
    * main - prints the name of the file it was compiled from
     * Return: 0
     */
-int main (void)
+int main(void)
 {
-	printf("%s\n",__BASE_FILE__);
+	printf("%s\n", __BASE_FILE__);
 	return (0);
 }
 

7e6f7209c3003e4916f68cf4b39e16aae42dbf43
kholoud9920 <kholoud9920@gmail.com>
2022-11-15 08:53:09 -0800

    m

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+   * @name: the name of the dog
+   * @age: the age of the dog
+    * @owner: the owner of the dog
+     * Return: nothing
+    */
+struct dog {
+	char * name;
+	float age;
+	char* owner
+};
